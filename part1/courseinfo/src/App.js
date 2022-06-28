56741d77b3e82b2885b760d2366e7387ae8341e1
b05902132 <b05902132@csie.ntu.edu.tw>
2022-06-29 01:31:19 +0800

    part1.a exercises done.

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,3 +1,31 @@
+const Header = (prop) => (
+  <h1> {prop.course} </h1>
+)
+
+const Part = (prop) => (
+  <p>
+    {prop.name} {prop.exercise}
+  </p>
+)
+
+const Content = (prop) => {
+  const courses = prop.courses;
+  return (
+    <div>
+      <Part name={courses[0].name} exercise={courses[0].exercises} />
+      <Part name={courses[1].name} exercise={courses[1].exercises} />
+      <Part name={courses[2].name} exercise={courses[2].exercises} />
+    </div>
+  );
+}
+
+const Total = (prop) => {
+  const total = prop.courses.reduce((total, x) => total + x.exercises, 0);
+  return (
+      <p>Number of exercises {total}</p>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -6,20 +34,17 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  const courses = [
+    {name: part1, exercises: exercises1},
+    {name: part2, exercises: exercises2},
+    {name: part3, exercises: exercises3}
+  ];
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header course={course} />
+      <Content courses={courses} />
+      <Total courses={courses} />
     </div>
   )
 }
