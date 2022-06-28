bad378967163dab0c2a5d93cca38b00e308b51ca
b05902132 <b05902132@csie.ntu.edu.tw>
2022-06-29 03:38:46 +0800

    unicafe done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,4 +1,60 @@
 import { useState } from 'react'
+const Header = ({text}) => (<h1>{text}</h1>)
+
+const Button = ({onClick, text}) => (
+  <button onClick={onClick}>
+    {text}
+  </button>
+)
+
+const StatisticLine = ({text, value}) => (
+  <tr>
+    <td>{text}</td><td>{value}</td>
+  </tr>
+)
+
+const Table = ({entries}) => {
+  const rows = Array.from(entries).map(([k, v]) => (
+    <StatisticLine id={k} text={k} value={v} />
+  ));
+  return (
+    <table>
+      <tbody>
+        {rows}
+      </tbody>
+    </table>
+  )
+
+}
+
+const Statistics = ({stats}) => {
+  const {good, neutral, bad} = stats;
+  let all = good + neutral + bad;
+  if (all === 0) {
+    return (
+      <div>
+        <Header text="Statistics" />
+        <p> No feedback given </p>
+      </div>
+    )
+  }
+  let average = (good - bad) / 3;
+  let positive = (good / all * 100).toString() + '%';
+  let entries = new Map();
+  entries.set("good", good);
+  entries.set("neutral", neutral);
+  entries.set("bad", bad);
+  entries.set("all", all);
+  entries.set("average", average);
+  entries.set("positive", positive);
+  return (
+    <div>
+      <Header text="Statistics" />
+      <Table entries={entries} />
+    </div>
+  )
+
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -8,7 +64,15 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <div>
+        <Header text="give feedback" />
+        <div>
+          <Button onClick={() => setGood(good + 1)} text="good" />
+          <Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
+          <Button onClick={() => setBad(bad + 1)} text="bad" />
+        </div>
+      </div>
+      <Statistics stats={{good: good, neutral: neutral, bad: bad}} />
     </div>
   )
 }
