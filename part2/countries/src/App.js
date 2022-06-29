1ca768fee2f046ad52ca573278cd9b6ddfb54383
b05902132 <b05902132@csie.ntu.edu.tw>
2022-06-30 07:30:23 +0800

    countries: step3 done.

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,13 +1,48 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const openWeatherApiKey = process.env.REACT_APP_OPEN_WEATHER_API_KEY
+
+const getWeather = (location) => axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${openWeatherApiKey}`).then(s => s.data)
+
+const DisplayWeather = ({weather}) => {
+  const main = weather.weather[0].main;
+  const description = weather.weather[0].description;
+  const icon = weather.weather[0].icon;
+  const iconurl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+  const temperature = weather.main.temp;
+
+  return (
+    <div>
+      <h3> Weather in {weather.name} </h3>
+      <img src={iconurl} />
+      <p> {main}: {description} </p>
+      <p> temprature: {temperature} Celcius </p>
+      <p> wind speed: {weather.wind.speed} m/s </p>
+    </div>
+  )
+
+}
+
 const DisplayCountry = ({country}) => {
+  const [weather, setWeather] = useState(null);
+  useEffect(() => {
+    if(country != null){
+      console.log('fired: ', country.name.common);
+      getWeather(`${country.capital}`)
+        .then(data => {
+          console.log(`weather get: ${country.name.common}`)
+          console.log(data);
+          setWeather(data);
+        });
+    }
+  }, [country && country.name.common])
   const validCountry = country ?? false;
   if (!validCountry) {
     return <></>
-
   }
   const languages = Object.entries(country.languages).map(([k, s]) => <li key={k}> {s} </li>)
+  const weatherInfo = (weather ?? weather.name == country.capital) ? <DisplayWeather weather={weather} />: null;
   return (
     <article>
       <h2> {country.name.common} </h2>
@@ -22,6 +57,7 @@ const DisplayCountry = ({country}) => {
         </div>
       </section>
       <img src={country.flags.svg} alt="flag" />
+      {weatherInfo}
     </article>
   )
 }
