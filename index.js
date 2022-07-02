bb09c807c337ff219db8baea983a80f4f91e7117
b05902132 <b05902132@csie.ntu.edu.tw>
2022-07-02 19:49:33 +0800

    Extracting router

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 'use strict'
 
 const config = require('./utils/config')
+const bloglistRouter = require('./controller/bloglist')
 const mongoose = require('mongoose')
 
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-
-const Blog = require('./models/Blog')
 
 const logger = require('./utils/logger')
 
@@ -25,23 +24,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', bloglistRouter)
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
