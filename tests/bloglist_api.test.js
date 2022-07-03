cab8d03a52ca2238b958a94dbd82de0e78329610
b05902132 <b05902132@csie.ntu.edu.tw>
2022-07-04 02:05:09 +0800

    Organize tests

--- tests/bloglist_api.test.js
+++ tests/bloglist_api.test.js
@@ -12,80 +12,83 @@ beforeEach(async () => {
   await Blog.create(helper.initialBlogs)
 })
 
-test('Blogs are returned as json', async () => {
-  await api.get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('Get blog list', () => {
+  test('Blogs are returned as json', async () => {
+    await api.get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+
+  test('All blogs are returend', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+
+  test('Blogs have an id', async () => {
+    const response = await api.get('/api/blogs')
+    const blogs = response.body
+    blogs.forEach(b => expect(b.id).toBeDefined())
+    blogs.forEach(b => expect(b._id).toBeUndefined())
+  })
 })
 
-test('All blogs are returend', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
+describe('Posting a new blog', () => {
+  test('Posted blog is returned correctly', async () => {
+    const newBlog = {
+      title: 'title',
+      author: 'author',
+      url: 'http://localhost/',
+      likes: 0
+    }
 
-test('Blogs have an id', async () => {
-  const response = await api.get('/api/blogs')
-  const blogs = response.body
-  blogs.forEach(b => expect(b.id).toBeDefined())
-  blogs.forEach(b => expect(b._id).toBeUndefined())
-})
+    const response = await api.post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+    const receivedBlog = response.body
+    expect(receivedBlog).toMatchObject(newBlog)
+  })
 
+  test('Posted blog is updated to the db', async () => {
+    const beforeBlogs = await helper.currentBlogs()
+    const newBlog = {
+      title: 'title',
+      author: 'author',
+      url: 'http://localhost/',
+      likes: 0
+    }
+    const response = await api.post('/api/blogs').send(newBlog)
+    const newId = response.body.id
 
-test('Posted blog is returned correctly', async () => {
-  const newBlog = {
-    title: 'title',
-    author: 'author',
-    url: 'http://localhost/',
-    likes: 0
-  }
+    const afterBlogs = await helper.currentBlogs()
+    expect(afterBlogs.length).toBe(beforeBlogs.length + 1)
+    const extraBlog = afterBlogs.find(o => o.id === newId)
+    expect(extraBlog).toMatchObject(newBlog)
+  })
 
-  const response = await api.post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-  const receivedBlog = response.body
-  expect(receivedBlog).toMatchObject(newBlog)
-})
+  test('If new blog has no likes, it defaults to 0', async () => {
+    const newBlog = {
+      title: 'title',
+      author: 'author',
+      url: 'http://localhost/'
+    }
+    const response = await api.post('/api/blogs')
+      .send(newBlog)
+      .expect(201) // created succesffuly
+    expect(response.body.likes).toBe(0)
+  })
 
-test('Posted blog is updated to the db', async () => {
-  const beforeBlogs = await helper.currentBlogs()
-  const newBlog = {
-    title: 'title',
-    author: 'author',
-    url: 'http://localhost/',
-    likes: 0
-  }
-  const response = await api.post('/api/blogs').send(newBlog)
-  const newId = response.body.id
-
-  const afterBlogs = await helper.currentBlogs()
-  expect(afterBlogs.length).toBe(beforeBlogs.length + 1)
-  const extraBlog = afterBlogs.find(o => o.id === newId)
-  expect(extraBlog).toMatchObject(newBlog)
-})
-
-test('If new blog has no likes, it defaults to 0', async () => {
-  const newBlog = {
-    title: 'title',
-    author: 'author',
-    url: 'http://localhost/'
-  }
-  const response = await api.post('/api/blogs')
-    .send(newBlog)
-    .expect(201) // created succesffuly
-  expect(response.body.likes).toBe(0)
-})
-
-test('Do not accept blogs without title and url', async () => {
-  const oldBlogs = await helper.currentBlogs()
-  const newBlog = {
-    author: 'author'
-  }
-  await api.post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
-  const newBlogs = await helper.currentBlogs()
-  expect(newBlogs).toEqual(oldBlogs)
+  test('Do not accept blogs without title and url', async () => {
+    const oldBlogs = await helper.currentBlogs()
+    const newBlog = {
+      author: 'author'
+    }
+    await api.post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+    const newBlogs = await helper.currentBlogs()
+    expect(newBlogs).toEqual(oldBlogs)
+  })
 })
 
 afterAll(() => mongoose.connection.close())
