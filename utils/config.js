f16335ebaf6f0b642218061c8c42a3294b40f272
b05902132 <b05902132@csie.ntu.edu.tw>
2022-07-04 00:23:36 +0800

    Prepare for testing environment

--- utils/config.js
+++ utils/config.js
@@ -3,6 +3,11 @@
 require('dotenv').config()
 
 const PORT = process.env.PORT || 3003
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/bloglist'
+const MONGODB_URI =
+  (
+    process.env.NODE_ENV === 'test'
+      ? process.env.TEST_MONGODB_URI
+      : process.env.MONGODB_URI
+  ) || 'mongodb://localhost/bloglist'
 
 module.exports = { PORT, MONGODB_URI }
