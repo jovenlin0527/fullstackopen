8f675a18f4030da6b8d44f096a6c97aa024b644c
b05902132 <b05902132@csie.ntu.edu.tw>
2022-07-21 22:25:49 +0800

    Extract BlogForm

---
+++ src/components/BlogForm.js
@@ -0,0 +1,31 @@
+import { useRef } from 'react'
+import PropTypes from 'prop-types'
+
+import TextField from './TextField'
+
+
+const BlogForm = ({ submitBlog }) => {
+  const submit = (event) => {
+    event.preventDefault()
+    const { title, author, url } = event.target
+    fieldRefs.forEach(x => x.current.clear())
+    return submitBlog({ title: title.value, author: author.value, url: url.value })
+  }
+  const fieldRefs = [useRef(), useRef(), useRef()]
+  return (
+    <div>
+      <form onSubmit={submit}>
+        <TextField name='title' prompt='title: ' ref={fieldRefs[0]} />
+        <TextField name='author' prompt='author: ' ref={fieldRefs[1]} />
+        <TextField name='url' prompt='url: ' ref={fieldRefs[2]} />
+        <input type='submit' />
+      </form>
+    </div>
+  )
+}
+
+BlogForm.propTypes = {
+  submitBlog: PropTypes.func.isRequired,
+}
+
+export default BlogForm
--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,35 +1,9 @@
 import { useRef } from 'react'
 import PropTypes from 'prop-types'
 
-import TextField from './TextField'
 import Togglable from './Togglable'
 import Blog, { BlogType } from './Blog'
-
-
-
-const BlogForm = ({ submitBlog }) => {
-  const submit = (event) => {
-    event.preventDefault()
-    const { title, author, url } = event.target
-    fieldRefs.forEach(x => x.current.clear())
-    return submitBlog({ title: title.value, author: author.value, url: url.value })
-  }
-  const fieldRefs = [useRef(), useRef(), useRef()]
-  return (
-    <div>
-      <form onSubmit={submit}>
-        <TextField name='title' prompt='title: ' ref={fieldRefs[0]} />
-        <TextField name='author' prompt='author: ' ref={fieldRefs[1]} />
-        <TextField name='url' prompt='url: ' ref={fieldRefs[2]} />
-        <input type='submit' />
-      </form>
-    </div>
-  )
-}
-
-BlogForm.propTypes = {
-  submitBlog: PropTypes.func.isRequired,
-}
+import BlogForm from './BlogForm'
 
 const BlogList = ({ username, header, blogs, submitBlog, likeBlog, deleteBlog }) => {
   const formRef = useRef()
